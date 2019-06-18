2224e12951dc6e554c06291693979be08b73d07a
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-06-18 10:32:08 +0200

    Fixed installation, added basic cli.

--- workspace/src/rosis/src/main.cpp
+++ workspace/src/rosis/src/main.cpp
@@ -1,12 +1,22 @@
 #include <soss/Instance.hpp>
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main(int argc, char* argv[])
 {
+    std::vector<std::string> args(argv, argv + argc);
+
+    if (argc == 1 || (argc >= 2 && (args[1] == "-h" || args[1] == "--help")))
+    {
+        std::cout << "Usage: rosis <config_file.yaml> | -h | --help " << std::endl;
+        return argc != 1;
+    }
+
     std::cout << "Hello rosis" << std::endl;
 
-    const YAML::Node config_node;
+    YAML::Node config_node;
 
     return soss::run_instance(config_node).wait();
 }
