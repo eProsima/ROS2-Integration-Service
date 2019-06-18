f4aceb27fb2542314f094b47d8fcaa81cc75488a
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-06-18 14:44:02 +0200

    Yaml transformation rosis -> soss.

--- workspace/src/rosis/src/main.cpp
+++ workspace/src/rosis/src/main.cpp
@@ -14,9 +14,41 @@ int main(int argc, char* argv[])
         return argc != 1;
     }
 
-    std::cout << "Hello rosis" << std::endl;
+    YAML::Node rosis_config = YAML::LoadFile(args[1]);
 
-    YAML::Node config_node;
+    YAML::Node soss_config;
+    soss_config["systems"]["ros2"] = YAML::Load("type: ros2");
 
-    return soss::run_instance(config_node).wait();
+    for (const YAML::detail::iterator_value& plugin: rosis_config["plugins"])
+    {
+        soss_config["systems"][plugin.first]["type"] = plugin.second["type"];
+    }
+
+    for (const YAML::detail::iterator_value& comm: rosis_config["topics"])
+    {
+        YAML::Node route;
+        if (comm.second["to"])
+        {
+            route["from"] = "ros2";
+            route["to"] = comm.second["to"];
+        }
+        else if (comm.second["from"])
+        {
+            route["from"] = comm.second["from"];
+            route["to"] = "ros2";
+        }
+        else
+        {
+            std::cerr << "Error: a 'communication' must have 'to' or 'from' value" << std::endl;
+            return 1;
+        }
+
+        soss_config["topics"][comm.first]["type"] = comm.second["type"];
+        soss_config["topics"][comm.first]["route"] = route;
+    }
+
+    //std::cout << "\n===== SOSS CONFIG =====" << std::endl;
+    //std::cout << soss_config << std::endl;
+
+    return soss::run_instance(soss_config).wait();
 }
